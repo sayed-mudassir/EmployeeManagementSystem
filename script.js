b92e56b12ac9765ad5e01ad9fd312dc240f9b83c
sayed-mudassir <sayedmuddassir68@gmail.com>
2024-05-22 02:12:27 +0530

    added edit functionality

--- script.js
+++ script.js
@@ -3,27 +3,75 @@ const modal = document.getElementById("modal");
 const closeIcon = document.getElementById("close-icon");
 const form = document.getElementById("form");
 const tableBody = document.querySelector("#employee-list > tbody");
+const updateModal = document.getElementById("modal1");
+const updateForm = document.getElementById("updateForm");
+const closeUpdateIcon = document.getElementById("close-icon2");
+
 function toggelModal(){
     modal.classList.toggle("hide-modal");
     modal.classList.toggle("show-modal");
 }
+
 let id = 1;
 const employees = {};
 function getNewId(){
     return id++;
 }
+
 function deleteRecord(e){
     const deleteButton = e.target;
     const td = deleteButton.parentNode;
     const tr = td.parentNode;
     tr.remove(); 
 }
-function editRecord(e){
-    
-    // under porcess 
+
+let editingEmployeeId = null;
+function toggelUpadteModal(){
+    updateModal.classList.toggle("hide-modal");
+    updateModal.classList.toggle("show-modal");
 }
+function prefill(employees){
+    for(let i in employees){
+        updateForm[i] && (updateForm[i].value = employees[i])
+    }
+}
+function editRecord(e){
+    const empId = e.target.parentNode.parentNode.id;
+    editingEmployeeId = empId;
+    toggelUpadteModal();
+    prefill(employees[empId]);
+    console.log(employees[empId]); 
+}
+updateForm.addEventListener("submit",(e) => {
+    e.preventDefault();
+
+    const updatedInfo = {
+        name : e.target.name.value,
+        email: e.target.email.value,
+        id: editingEmployeeId,
+        role: e.target.role.value,
+        DOJ: e.target.DOJ.value,
+        gender: e.target.gender.value,
+        
+    }
+
+    employees[editingEmployeeId] = updatedInfo;
+
+    updateForm.reset();
+    toggelUpadteModal();
+
+    const record = document.getElementById(editingEmployeeId);
+    let tdCellIndex = 0;
+    for(let i in updatedInfo){
+        record.children[tdCellIndex++].innerText = updatedInfo[i];
+    }
+});
+
 function createNewEmployeeList(e){
+    // creating new row for printing new employee detail
     const record = document.createElement("tr");
+    record.id = e.id;
+    // this loop is used to creating new column to fill the details of new employee
     for(let key in e){
         const cell = document.createElement("td");
         cell.innerText = e[key];
@@ -65,4 +113,4 @@ form.addEventListener("submit",(e)=>{
 })
 modalToggelButton.addEventListener("click",toggelModal);
 closeIcon.addEventListener("click",toggelModal);
-
+closeUpdateIcon.addEventListener("click",toggelUpadteModal);
